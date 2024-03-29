ffbb4a3bfeada98f62676ac97b6226323c4d7c94
ElashryClinic <elashryclinic@gmail.com>
2024-03-29 22:06:23 +0200

    Works right now

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+function sendEmail()
+{
 Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "elashryclinic@gmail.com",
-    Password : "elashryclinic@@..ElManial",
-    To : 'hmztalshry98@gmail.com',
-    From : "elashryclinic@gmail.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
-}).then(
-  message => alert(message)
-);
+    Host: "smtp.elasticemail.com",
+    Username: "elashryclinic@gmail.com",
+    Password: "719D95FB23012B20FE8CFA4667E8C3369096",
+    To: "elashryclinic@gmail.com",
+    From: "elashryclinic@gmail.com",
+    Subject: "رسالة جديدة",
+    Body: `توجد رسالة جديدة بالتفاصيل التالية:<br>الاسم: ${document.getElementById('name').value}<br>الرسالة: ${document.getElementById('msg').value}`
+}).then(message => alert(message));
+}
